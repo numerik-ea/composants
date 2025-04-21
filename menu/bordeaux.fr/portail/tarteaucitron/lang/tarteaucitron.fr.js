d94abca4f012eff671a258a40d481eaae392645b
Florent Roques <florentroques@users.noreply.github.com>
2025-04-21 21:04:27 +0200

    Ajout du composant menu bordeaux.fr

---
+++ menu/bordeaux.fr/portail/tarteaucitron/lang/tarteaucitron.fr.js
@@ -0,0 +1,96 @@
+/*global tarteaucitron */
+tarteaucitron.lang = {
+
+    /* Specificites bordeaux.fr */
+    "engage-calameo" : "Autoriser le dépôt d'un cookie pour afficher ce contenu.",
+    "engage-comarquage" : "Autoriser le dépôt d'un cookie pour afficher ce contenu.",
+    "engage-dailymotionplayerbdxfr" : "Autoriser le dépôt d'un cookie pour afficher ce contenu.",
+    "engage-dailymotionjukebox" : "Autoriser le dépôt d'un cookie pour afficher ce contenu.",
+    "engage-dailymotionwidget" : "Autoriser le dépôt d'un cookie pour afficher ce contenu.",
+    "engage-lightwidget" : "Autoriser le dépôt d'un cookie pour afficher ce contenu.",
+    "engage-mailperf" : "Autoriser le dépôt d'un cookie pour afficher ce contenu.",
+    "engage-recaptcha" : "Autoriser le dépôt d'un cookie pour afficher ce contenu.",
+    "engage-viewsurf" : "Autoriser le dépôt d'un cookie pour afficher ce contenu.",
+    "engage-facebookvideoplayer" : "Autoriser le dépôt d'un cookie pour afficher ce contenu.",
+    "engage-youtube" : "Autoriser le dépôt d'un cookie pour afficher ce contenu.",
+    "engage-youtubeplaylist" : "Autoriser le dépôt d'un cookie pour afficher ce contenu.",
+    "engage-soundcloud" : "Autoriser le dépôt d'un cookie pour afficher ce contenu.",
+
+    "middleBarHead": "☝ 🍪",
+    "adblock": "Bonjour! Ce site joue la transparence et vous donne le choix des services tiers à activer.",
+    "adblock_call": "Merci de désactiver votre adblocker pour commencer la personnalisation.",
+    "reload": "Recharger la page",
+    
+    "alertBigScroll": "En continuant de défiler,",
+    "alertBigClick": "En poursuivant votre navigation,",
+    "alertBig": "vous acceptez l'utilisation de services tiers pouvant installer des cookies",
+    
+    "alertBigPrivacy": "Le site Bordeaux.fr utilise des cookies pour améliorer votre expérience d’utilisation<br />ainsi que pour vous fournir des services via des contenus animés et interactifs de réseaux sociaux et vidéos. Vous pouvez à tout moment faire part de vos choix :",
+    "alertSmall": "Gestion des services",
+    "acceptAll": "Tout accepter",
+    "personalize": "En savoir plus et personnaliser",
+    "close": "Fermer",
+
+    "privacyUrl": "Politique de confidentialité",
+
+    "all": "Préférences pour tous les services",
+
+    "info": "Protection de votre vie privée",
+    "disclaimer": "Le site Bordeaux.fr utilise des cookies pour améliorer votre expérience d’utilisation ainsi que pour vous fournir des services via des contenus animés et interactifs de réseaux sociaux et vidéos. Ces cookies provenant de tiers collecteront et utiliseront vos données de navigation pour des finalités qui leur sont propres. Pour plus d’information, vous pouvez consulter leur politique de confidentialité.<br />Cette fenêtre vous permet de donner ou retirer votre consentement, soit globalement (en cliquant sur le bouton \"Tout Accepter\" ou \"Tout Refuser\"), soit pour chaque finalité. Nous conservons votre choix pendant 6 mois. Vous pouvez changer d’avis à tout moment en cliquant sur \"Gestion des cookies\" situé en bas à droite du site.<br />Pour en savoir plus sur le traitement des données personnelles par la ville de Bordeaux, vous pouvez consulter <a href=\"https://www.bordeaux.fr/p139044\" title=\"Politique de confidentialité de la ville de Bordeaux\">notre politique de confidentialité.</a>",
+    "allow": "Autoriser",
+    "deny": "Interdire",
+    "noCookie": "Ce service ne dépose aucun cookie.",
+    "useCookie": "Ce service peut déposer",
+    "useCookieCurrent": "Ce service a déposé",
+    "useNoCookie": "Ce service n'a déposé aucun cookie.",
+    "more": "En savoir plus",
+    "source": "Voir leur politique de confidentialité",
+    "credit": "Gestion des cookies par tarteaucitron.js",
+    "noServices": "Ce site n'utilise aucun cookie nécessitant votre consentement.",
+
+    "toggleInfoBox": "Afficher/masquer les informations sur le stockage des cookies",
+    "title": "GESTION DES COOKIES",
+    "cookieDetail": "Détail des cookies",
+    "ourSite": "sur notre site",
+    "newWindow": "(nouvelle fenêtre)",
+    "allowAll": "Tout accepter",
+    "denyAll": "Tout refuser",
+    
+    "fallback": "est désactivé.",
+
+    "ads": {
+        "title": "Régies publicitaires",
+        "details": "Les régies publicitaires permettent de générer des revenus en commercialisant les espaces publicitaires du site."
+    },
+    "analytic": {
+        "title": "Mesure d'audience",
+        "details": "Les services de mesure d'audience permettent de générer des statistiques de fréquentation utiles à l'amélioration du site."
+    },
+    "social": {
+        "title": "Réseaux sociaux",
+        "details": "Les réseaux sociaux permettent d'améliorer la convivialité du site et aident à sa promotion via les partages."
+    },
+    "video": {
+        "title": "Vidéos",
+        "details": "Les services vidéos permettent d'enrichir le site de contenu multimédia."
+    },
+    "comment": {
+        "title": "Commentaires",
+        "details": "Les gestionnaires de commentaires facilitent le dépôt de vos commentaires et luttent contre le spam."
+    },
+    "support": {
+        "title": "Support",
+        "details": "Les services de support vous permettent d'entrer en contact avec l'équipe du site et d'aider à son amélioration."
+    },
+    "api": {
+        "title": "APIs",
+        "details": "Les APIs permettent de charger des scripts : géolocalisation, moteurs de recherche, traductions, ..."
+    },
+    "other": {
+        "title": "Autres",
+        "details": "Services visant à afficher du contenu web."
+    },
+    
+    "mandatoryTitle": "Cookies obligatoires",
+    "mandatoryText": "Ce site utilise des cookies nécessaires à son bon fonctionnement qui ne peuvent pas être désactivés."
+};
